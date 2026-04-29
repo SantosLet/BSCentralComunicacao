0e2e1951c1e5aba3be0869bd8f9477bb4adce42d
Letícia Santos <dev.leticiasantos@gmail.com>
2026-04-29 08:11:09 -0300

    chore: Criar e estruturar projeto

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/no-unused-components': 'warn',
+      'vue/no-mutating-props': 'error',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bs-communications-center">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
